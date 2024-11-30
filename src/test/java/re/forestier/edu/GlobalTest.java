9e479f8ee9c82eb56a5dfb39933f4487d4c635d2
Baye Serigne Seck <Bayeserigneseck40@gmail.com>
2024-11-30 13:52:15 +0100

    commit change

--- src/test/java/re/forestier/edu/GlobalTest.java
+++ src/test/java/re/forestier/edu/GlobalTest.java
@@ -7,8 +7,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import org.junit.jupiter.api.Test;
 
+import static re.forestier.edu.rpg.Affichage.afficherEnMarkdown;
 import re.forestier.edu.rpg.Item;
 import re.forestier.edu.rpg.PlayerAdventurer;
+import re.forestier.edu.rpg.player;
 
 
 public class GlobalTest {
@@ -36,10 +38,17 @@ void testAffichage() {
     PlayerAdventurer player = new PlayerAdventurer("Florian", "Gnognak le Barbare", 200, new ArrayList<>());
     player.addXp(20);
     Item sword = new Item("Sword", "A sharp blade", 2, 100);
-
     player.inventory.add(sword);
     String resultat = player.afficherJoueur();
-
+    assertThat(resultat, containsString("Sword"));
+}
+@Test
+void testafficherEnMarkdown() {
+    player player = new PlayerAdventurer("Florian", "Gnognak le Barbare", 200, new ArrayList<>());
+    player.addXp(20);
+    Item sword = new Item("Sword", "A sharp blade", 2, 100);
+    player.inventory.add(sword);
+    String resultat =afficherEnMarkdown(player);
     assertThat(resultat, containsString("Sword"));
 }
 
