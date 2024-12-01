b508fb516c877d503006fb4be375a5c6c4bcbf7f
Baye Serigne Seck <Bayeserigneseck40@gmail.com>
2024-12-01 23:25:27 +0100

    commit change

--- src/main/java/re/forestier/edu/Main.java
+++ src/main/java/re/forestier/edu/Main.java
@@ -2,7 +2,9 @@ package re.forestier.edu;
 import java.util.ArrayList;
 
 import re.forestier.edu.rpg.Affichage;
+import re.forestier.edu.rpg.Item;
 import re.forestier.edu.rpg.PlayerAdventurer;
+import re.forestier.edu.rpg.PlayerGoblin;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,5 +18,27 @@ public class Main {
         System.out.println(firstPlayer.afficherJoueur());
         System.out.println("------------------");
         System.out.println(Affichage.afficherEnMarkdown(firstPlayer));
+
+        PlayerGoblin player = new PlayerGoblin("Florian", "Gnognak l'Aventurier", 100, new ArrayList<>());
+        player.money = 50; 
+        Item sword = new Item("Sword", "A sharp blade", 10, 100);
+        Item shield = new Item("Shield", "A sturdy shield", 15, 150);
+        player.inventory.add(sword);
+        player.inventory.add(shield);
+        System.out.println("Inventaire initial :");
+        player.inventory.forEach(item -> System.out.println("- " + item.getName()));
+        System.out.println("Argent initial : " + player.money);
+        System.out.println("\nTentative de vendre l'objet 'Sword'...");
+        boolean success = player.sellItem("Sword");
+        System.out.println("Résultat de la vente : " + (success ? "Succès" : "Échec"));
+        System.out.println("Argent après vente : " + player.money);
+        System.out.println("\nInventaire après la vente de 'Sword' :");
+        player.inventory.forEach(item -> System.out.println("- " + item.getName()));
+        System.out.println("\nTentative de vendre l'objet 'Bow'...");
+        success = player.sellItem("Bow");
+        System.out.println("Résultat de la vente : " + (success ? "Succès" : "Échec"));
+        System.out.println("Argent après tentative de vente : " + player.money);
+        System.out.println("\nInventaire final :");
+        player.inventory.forEach(item -> System.out.println("- " + item.getName()));
     }
 }
